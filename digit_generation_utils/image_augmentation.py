7ca1306af200eca4e3de5500ef88ed6042e83efb
redowan <redowan@shopf.co>
2019-04-02 12:10:58 +0600

    removed digit flipping

--- digit_generation_utils/image_augmentation.py
+++ digit_generation_utils/image_augmentation.py
@@ -118,14 +118,13 @@ def augmentation(folder, sample=100):
     p.add_operation(blur)
     p.add_operation(noise)
 
-    p.rotate90(probability=0.05)
-    p.rotate270(probability=0.05)
-    p.crop_by_size(probability=0.5, width=250, height=250)
+    #p.black_and_white(probability = 1, threshold = 128)
+    p.rotate90(probability=0.001)
+    p.rotate270(probability=0.001)
+    p.rotate(probability = 0.3, max_left_rotation = 25, max_right_rotation = 25)
     p.crop_random(probability=0.3, percentage_area = 0.9)
     p.zoom(probability=0.1, min_factor=1.01, max_factor=1.03)
 
-    p.flip_left_right(probability = 0.1)
-    p.flip_top_bottom(probability = 0.1)
 
     p.skew_tilt(probability = 0.03, magnitude = 1)
     p.skew_left_right(probability = 0.03, magnitude = 1)
